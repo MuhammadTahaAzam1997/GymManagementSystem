49d5e83cfcdd541395598c706088b88c9892f6c7
Muhammad Taha Azam <30570019+MuhammadTahaAzam1997@users.noreply.github.com>
2024-05-13 22:32:33 +0500

    new file

--- Database/gym.sql
+++ Database/gym.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 16, 2023 at 07:20 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.9
+-- Generation Time: May 13, 2024 at 07:16 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,17 +31,19 @@ CREATE TABLE `billing` (
   `id` varchar(20) NOT NULL,
   `name` varchar(20) NOT NULL,
   `date` varchar(20) NOT NULL,
-  `amount` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `amount` varchar(20) NOT NULL,
+  `mid` int(10) NOT NULL,
+  `rid` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `billing`
 --
 
-INSERT INTO `billing` (`id`, `name`, `date`, `amount`) VALUES
-('1', 'Muhammad Taha Azam', '12-06-2022', '5000'),
-('1', 'Taha Azam', '12-06-2022', '5000'),
-('2', 'Sara', '12-06-2022', '45000');
+INSERT INTO `billing` (`id`, `name`, `date`, `amount`, `mid`, `rid`) VALUES
+('1', 'Muhammad Taha Azam', '12-06-2022', '5000', 0, 0),
+('1', 'Taha Azam', '12-06-2022', '5000', 0, 0),
+('2', 'Sara', '12-06-2022', '45000', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -54,7 +56,7 @@ CREATE TABLE `coach` (
   `name` varchar(20) NOT NULL,
   `date` varchar(20) NOT NULL,
   `experience` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `coach`
@@ -75,15 +77,16 @@ CREATE TABLE `member` (
   `date` varchar(20) NOT NULL,
   `dob` varchar(20) NOT NULL,
   `phone` varchar(20) NOT NULL,
-  `coach` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `coach` varchar(20) NOT NULL,
+  `cid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `member`
 --
 
-INSERT INTO `member` (`id`, `name`, `date`, `dob`, `phone`, `coach`) VALUES
-('1', 'Taayyab', '06 August 2012', '21-july-1998', '03422506772', 'john');
+INSERT INTO `member` (`id`, `name`, `date`, `dob`, `phone`, `coach`, `cid`) VALUES
+('1', 'Taayyab', '06 August 2012', '21-july-1998', '03422506772', 'john', 0);
 
 -- --------------------------------------------------------
 
@@ -97,7 +100,7 @@ CREATE TABLE `receptionist` (
   `date` varchar(20) NOT NULL,
   `address` varchar(20) NOT NULL,
   `phone` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `receptionist`
